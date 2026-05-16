5a1850b7cd3421c6948b8716578b115fc3a9ed5e
jalhhhffuy <gamal234567894@gmail.com>
2026-05-16 20:12:30 +0300

    Implement Google login and callback routes
    Add Google authentication routes for login and callback

--- index.js
+++ index.js
@@ -40,3 +40,35 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+// تسجيل دخول قوقل
+app.get("/auth/google", (req, res) => {
+
+    const clientId = "218711384600-6dc1biu2g1kmh9rk6n3bd9uvq8kpmsjs.apps.googleusercontent.com";
+
+    const redirectUri =
+        "https://my-server-i40i.onrender.com/auth/google/callback";
+
+    const scope =
+        "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
+
+    const googleUrl =
+        "https://accounts.google.com/o/oauth2/v2/auth" +
+        "?client_id=" + clientId +
+        "&redirect_uri=" + encodeURIComponent(redirectUri) +
+        "&response_type=code" +
+        "&scope=" + encodeURIComponent(scope);
+
+    res.redirect(googleUrl);
+});
+
+// رجوع قوقل بعد تسجيل الدخول
+app.get("/auth/google/callback", async (req, res) => {
+
+    const code = req.query.code;
+
+    if (!code) {
+        return res.send("Google login failed");
+    }
+
+    res.send("تم تسجيل الدخول بقوقل بنجاح");
+});
